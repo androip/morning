b947ac5139d957d6f5b18a82b6f7c5b209735d1a
jiangpeng1 <iwalkman@163.com>
2019-04-10 21:32:35 +0800

    engine

---
+++ engine/engine_service/src/main/java/morning/service/impl/EngineService.java
@@ -0,0 +1,16 @@
+package morning.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EngineService {
+
+	public String startProcess(String processTemplateId) {
+		// TODO 调用Meta api 查询流程模版
+		// TODO 实例化一个流程实例 
+		// TODO 初始化流程实例的状态为Ready
+		// TODO 初始化Start节点状态为Running
+		return "XXid";
+	}
+
+}
---
+++ engine/engine_web/src/main/java/morning/web/EngineController.java
@@ -0,0 +1,44 @@
+package morning.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import morning.service.impl.EngineService;
+
+@Api(value = "Engin管理API接口")
+@RestController
+@RequestMapping(value="/engin")
+public class EngineController {
+
+	@Autowired
+	EngineService engineService;
+	
+	@ApiOperation(value = "指定流程模版，实例化一个流程", notes = "")
+	@RequestMapping(value = {"/engin/procins"}, method = RequestMethod.POST)
+	public String  getProcessTemplate(@RequestParam String processTemplateId) {
+		String processId = engineService.startProcess(processTemplateId);
+		return processId;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
